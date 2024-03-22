a831665ac85e6b3ef20a1a11995f14ee61fcb3ae
fernandezfran <ffernandev@gmail.com>
2024-03-22 13:55:32 -0300

    mv SysQuery to database module

--- database/__init__.py
+++ database/__init__.py
@@ -1,13 +1,20 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+import importlib
 import os
 import pathlib
+import re
+import subprocess
+import warnings
 
+import numpy as np
 import pandas as pd
 
 import database.liiondb.functions.fn_db as fn_db
 
 PATH = pathlib.Path(os.path.join(os.path.abspath(os.path.dirname(__file__))))
+
+warnings.filterwarnings("ignore", category=RuntimeWarning)
 
 
 def read_database(merge_on=["doi", "material"]):
@@ -49,6 +56,71 @@ def read_database(merge_on=["doi", "material"]):
         ]
     ]
 
-    database.reset_index(drop=True, inplace=True)
+    return database.reset_index(drop=True)
 
-    return database
+
+class SysQuery:
+    def __init__(self, sys, socs="linspace"):
+        self.sys = sys
+        self.socs = np.linspace(1e-4, 0.9999) if socs == "linspace" else socs
+
+    def _func_eval(self, func):
+        with open(PATH / "_func.py", "wb") as binf:
+            binf.write(func)
+
+        subprocess.run(["black", "--quiet", PATH / "_func.py"])
+
+        import database._func
+
+        importlib.reload(database._func)
+
+        try:
+            yvals = database._func.function(self.socs)
+        except TypeError:
+            yvals = database._func.function(self.socs, T=298)
+
+        os.remove(PATH / "_func.py")
+
+        return yvals
+
+    def _raw_data_value(self, value):
+        return float(value)
+
+    def _raw_data_array(self, array):
+        return pd.DataFrame(
+            data=[
+                (x, y) for x, y in re.findall(r"\{([\d.]+),([\d.]+)\}", array)
+            ],
+            dtype=float,
+        )
+
+    @property
+    def isotherm(self):
+        if self.sys["isotherm_function"] is None:
+            isotherm = self._raw_data_array(self.sys["isotherm"])
+        else:
+            voltage = self._func_eval(self.sys["isotherm_function"])
+            isotherm = pd.DataFrame({0: self.socs, 1: voltage})
+
+        return isotherm
+
+    @property
+    def particle_size(self):
+        return 1e2 * self._raw_data_value(self.sys["particle_size"])
+
+    @property
+    def dcoeff(self):
+        if self.sys["dcoeff_function"] is None:
+            try:
+                dcoeff = self._raw_data_value(self.sys["dcoeff"])
+            except ValueError:
+                dcoeffs = self._raw_data_array(self.sys["dcoeff"])
+                try:
+                    dcoeff = np.mean(dcoeffs.iloc[:, 1])
+                except IndexError:
+                    dcoeff = np.nan
+        else:
+            dcoeffs = self._func_eval(self.sys["dcoeff_function"])
+            dcoeff = np.mean(dcoeffs)
+
+        return 1e4 * dcoeff
--- main.py
+++ main.py
@@ -1,92 +1,24 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-import importlib
-import os
-import re
-import subprocess
-import warnings
-
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 
-from database import read_database
-
-
-class SysQuery:
-    def __init__(self, sys, socs="linspace"):
-        self.sys = sys
-        self.socs = np.linspace(0, 1) if socs == "linspace" else socs
-
-    def _func_eval(self, func):
-        with open("_func.py", "wb") as binf:
-            binf.write(func)
-
-        subprocess.run(["black", "--quiet", "_func.py"])
-
-        import _func
-
-        importlib.reload(_func)
-
-        try:
-            yvals = _func.function(self.socs)
-        except TypeError:
-            yvals = _func.function(self.socs, T=298)
-
-        os.remove("_func.py")
-
-        return yvals
-
-    def _raw_data_value(self, value):
-        return float(value)
-
-    def _raw_data_array(self, array):
-        return pd.DataFrame(
-            data=[
-                (x, y) for x, y in re.findall(r"\{([\d.]+),([\d.]+)\}", array)
-            ],
-            dtype=float,
-        )
-
-    @property
-    def isotherm(self):
-        if self.sys["isotherm_function"] is None:
-            isotherm = self._raw_data_array(self.sys["isotherm"])
-        else:
-            voltage = self._func_eval(self.sys["isotherm_function"])
-            isotherm = pd.DataFrame({0: self.socs, 1: voltage})
-
-        return isotherm
-
-    @property
-    def particle_size(self):
-        return 1e2 * self._raw_data_value(self.sys["particle_size"])
-
-    @property
-    def dcoeff(self):
-        if self.sys["dcoeff_function"] is None:
-            try:
-                dcoeff = self._raw_data_value(self.sys["dcoeff"])
-            except ValueError:
-                dcoeffs = self._raw_data_array(self.sys["dcoeff"])
-                try:
-                    dcoeff = np.mean(dcoeffs.iloc[:, 1])
-                except IndexError:
-                    dcoeff = np.nan
-        else:
-            dcoeffs = self._func_eval(self.sys["dcoeff_function"])
-            dcoeff = np.mean(dcoeffs)
-
-        return 1e4 * dcoeff
-
-
-warnings.filterwarnings("ignore", category=RuntimeWarning)
+from database import read_database, SysQuery
 
 database = read_database()
 
+dcoeffs = []
 for index, sys in database.iterrows():
     sysq = SysQuery(sys)
 
     isotherm = sysq.isotherm
     particle_size = sysq.particle_size
     dcoeff = sysq.dcoeff
+
+    if not np.isnan(dcoeff):
+        dcoeffs.append(dcoeff)
+
+plt.boxplot(dcoeffs)
+plt.yscale("log")
+plt.show()
