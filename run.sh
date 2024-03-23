1f8ce8e88cd7a5b19d09159057e271b4bbd8f1bf
fernandezfran <ffernandev@gmail.com>
2024-03-23 19:13:03 -0300

    run and gitignore updates

--- run.sh
+++ run.sh
@@ -2,8 +2,8 @@
 
 pip install -r requirements.txt
 
-git clone https://github.com/ndrewwang/liiondb.git database/liiondb
-cd database/liiondb
+git clone https://github.com/ndrewwang/liiondb.git api/database/liiondb
+cd api/database/liiondb
 git reset --hard ee38d6b
 pip install -r requirements.txt
 cd ../../
