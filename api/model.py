8798024af9c2a90fa44b13cf0e669efb28909d7c
fernandezfran <ffernandev@gmail.com>
2024-03-24 23:32:11 -0300

    add mask in the model fitting to avoid extreme repetitive values that weights in th fit

--- api/model.py
+++ api/model.py
@@ -5,7 +5,8 @@ import galpynostatic as gp
 
 def fit(c_rates, soc_maxs, particle_size):
     greg = gp.model.GalvanostaticRegressor(d=particle_size)
-    return greg.fit(c_rates.reshape(-1, 1), soc_maxs)
+    mask = (soc_maxs < 0.99) & (soc_maxs > 0.1)
+    return greg.fit(c_rates[mask].reshape(-1, 1), soc_maxs[mask])
 
 
 def dcoeff_prediction(greg):
